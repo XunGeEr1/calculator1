38ec3c9c5f934a503dc32a433728ce92828c740a
ning-kai <63397310+ning-kai@users.noreply.github.com>
2020-04-21 09:45:16 +0800

    版本1.0
    1.0

---
+++ calculator1.0/xiaodaishu/mainwindow.cpp
@@ -0,0 +1,28 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    connect(this->ui->calculator,SIGNAL(clicked(bool)),this,SLOT(on_calculator_clicked()));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::reshow(){
+    this->show();
+}
+
+void MainWindow::on_calculator_clicked()
+{
+        this->hide();//主界面关闭
+        calculator * d = new calculator;//新建子界面
+        connect(d,SIGNAL(sendsignal()),this,SLOT(reshow()));//当点击子界面时，调用主界面的reshow()函数
+        d->show();//子界面出
+
+}
